701df20769f90ef4c1926ef919dd8efdeb950171
xcheko51x <xcheko51x@gmail.com>
2022-04-22 13:30:39 -0500

    ADD - Ejemplo ciclo while, do while y uso de break

--- CicloWileDoWhile.java
+++ CicloWileDoWhile.java
@@ -1,21 +1,25 @@
 public class CicloWileDoWhile {
     public static void main(String[] args) {
         
-        int contador = 10;
+        int contador = 0;
 
         // Ciclo while
-        while(contador < 5) {
+        while(contador < 10) {
             System.out.println("Contador while: "+contador);
             contador++;
+            
+            if(contador == 5) {
+                break;
+            }
         }
 
-        contador = 10;
+        contador = 0;
 
         // Ciclo do while
         do {
             System.out.println("Contador do while: "+contador);
             contador++;
-        } while(contador < 5);
+        } while(contador < 10);
 
     }
 }
